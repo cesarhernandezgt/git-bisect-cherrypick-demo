b832812bc11d1a1ae60e7f515920a02efd56a1ef
CesarHernandezGt <cfhernan123@gmail.com>
2018-11-23 21:59:04 -0600

    commit D

--- src/main/java/com/blogspot/cesarhernandezgt/App.java
+++ src/main/java/com/blogspot/cesarhernandezgt/App.java
@@ -23,9 +23,14 @@ public class App {
     }
 
     public static int dividir(int a, int b) {
+        if (b != 0) {
             resultado = a / b;
             System.out.println(a + "/" + b + " = " + resultado);
             return resultado;
+        }else{
+            System.out.println(a + "/" + b + " = 0 (DIVISION ENTRE CERO MANEJADA!.");
+            return 0;
+        }
     }
 
 
